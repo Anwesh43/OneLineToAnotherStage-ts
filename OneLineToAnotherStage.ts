a0cabeb309f1140081387358f6ee89ffd6391011
Anwesh43 <anweshthecool0@gmail.com>
2018-07-28 15:35:48 +0530

    adding LinkedOTT class

--- OneLineToAnotherStage.ts
+++ OneLineToAnotherStage.ts
@@ -157,6 +157,30 @@ class OTTNode {
             return curr
         }
         cb()
-        return this 
+        return this
+    }
+}
+
+class LinkedOTT {
+
+    curr : OTTNode = new OTTNode(0)
+
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
     }
 }
